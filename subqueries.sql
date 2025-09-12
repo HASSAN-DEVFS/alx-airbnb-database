97ac1705cf0467a1383c3367ba30f78461f74333
HASSAN/SABBANI <hdjdjdjxjndj@gmail.com>
2025-09-12 18:26:49 +0100

    Update subqueries.sql

--- subqueries.sql
+++ subqueries.sql
@@ -6,3 +6,12 @@ WHERE (
     WHERE r.property_id = p.property_id
 ) > 4.0;
 
+
+
+SELECT u.user_id, u.name
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
