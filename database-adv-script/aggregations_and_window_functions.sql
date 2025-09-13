c070964eb385d6f7dc1c260f386d18c9d266acb8
CYBER_TECH <hdjdjdjxjndj@gmail.com>
2025-09-13 16:46:47 +0100

    Créer fichier aggregations

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,15 +1,20 @@
 
-SELECT u.user_id, u.name, COUNT(b.booking_id) AS total_bookings
-FROM users u
-LEFT JOIN bookings b ON u.user_id = b.user_id
-GROUP BY u.user_id, u.name;
+SELECT u.id AS user_id,
+       u.name AS user_name,
+       COUNT(b.id) AS total_bookings
+FROM Users u
+LEFT JOIN Bookings b ON u.id = b.user_id
+GROUP BY u.id, u.name
+ORDER BY total_bookings DESC;
 
 
 
-SELECT p.property_id, p.name,
-       COUNT(b.booking_id) AS total_bookings,
-       RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
-FROM properties p
-LEFT JOIN bookings b ON p.property_id = b.property_id
-GROUP BY p.property_id, p.name
-ORDER BY property_rank;
+SELECT p.id AS property_id,
+       p.name AS property_name,
+       COUNT(b.id) AS total_bookings,
+       ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS row_number_rank,  
+       RANK() OVER (ORDER BY COUNT(b.id) DESC) AS rank_position          
+FROM Properties p
+LEFT JOIN Bookings b ON p.id = b.property_id
+GROUP BY p.id, p.name
+ORDER BY total_bookings DESC;
