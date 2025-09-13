aa9373ae03603133b889c15eb7eb6de801a13a4b
CYBER_TECH <hdjdjdjxjndj@gmail.com>
2025-09-13 15:53:28 +0100

    Créer dossier et fichier joins-query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,7 +6,8 @@ SELECT b.id AS booking_id,
        u.name AS user_name,
        u.email
 FROM Bookings b
-INNER JOIN Users u ON b.user_id = u.id;
+INNER JOIN Users u ON b.user_id = u.id
+ORDER BY b.booking_date DESC;   -- ✅ ajout d’un ORDER BY
 
 
 
@@ -16,7 +17,9 @@ SELECT p.id AS property_id,
        r.rating,
        r.comment
 FROM Properties p
-LEFT JOIN Reviews r ON p.id = r.property_id;
+LEFT JOIN Reviews r ON p.id = r.property_id
+ORDER BY p.id;   -- ✅ tri par ID propriété
+
 
 
 SELECT u.id AS user_id,
@@ -25,4 +28,5 @@ SELECT u.id AS user_id,
        b.property_id,
        b.booking_date
 FROM Users u
-FULL OUTER JOIN Bookings b ON u.id = b.user_id;
+FULL OUTER JOIN Bookings b ON u.id = b.user_id
+ORDER BY u.id NULLS LAST;   -- ✅ tri par ID utilisateur
