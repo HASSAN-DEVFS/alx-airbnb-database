d3bbb60467b7bb6b19b03907612ee53ca25fd32d
CYBER_TECH <hdjdjdjxjndj@gmail.com>
2025-09-13 16:27:41 +0100

    Créer fichier subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,18 @@
+SELECT p.property_id, p.name
+FROM properties p
+WHERE (
+    SELECT AVG(r.rating)
+    FROM reviews r
+    WHERE r.property_id = p.property_id
+) > 4.0;
+
+
+
+SELECT u.user_id, u.name
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
+
