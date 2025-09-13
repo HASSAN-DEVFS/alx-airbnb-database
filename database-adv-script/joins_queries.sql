3cd88c83a5d994eedf9d2d63200b6b7b76b0781c
CYBER_TECH <hdjdjdjxjndj@gmail.com>
2025-09-13 15:45:33 +0100

    Créer dossier et fichier joins-query

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,28 @@
+
+SELECT b.id AS booking_id,
+       b.property_id,
+       b.booking_date,
+       u.id AS user_id,
+       u.name AS user_name,
+       u.email
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.id;
+
+
+
+SELECT p.id AS property_id,
+       p.name AS property_name,
+       r.id AS review_id,
+       r.rating,
+       r.comment
+FROM Properties p
+LEFT JOIN Reviews r ON p.id = r.property_id;
+
+
+SELECT u.id AS user_id,
+       u.name AS user_name,
+       b.id AS booking_id,
+       b.property_id,
+       b.booking_date
+FROM Users u
+FULL OUTER JOIN Bookings b ON u.id = b.user_id;
